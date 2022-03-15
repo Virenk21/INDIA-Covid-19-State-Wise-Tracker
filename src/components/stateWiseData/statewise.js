a8693f90206d1175ea1b16ea606dc5294ae7f167
Virenk21 <viren.d222111@gmail.com>
2022-03-16 04:00:07 +0530

    added api and table

---
+++ src/components/stateWiseData/statewise.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+
+const Statewise = () => {
+
+    const getCovidData = async () => {
+       const res = await fetch('https://api.rootnet.in/covid19-in/stats/latest');
+        const actualData = await res.json();
+        console.log(actualData.regional);
+    }
+
+    useEffect(() => {
+        getCovidData();
+    }, []);
+    
+
+  return (
+    <>
+      <h1> INDIA COVID-19 Dashboard</h1>
+      <div className="container-fluid mt-5">
+        <div className="main-heading">
+          <h1 className="mb-5">
+            {" "}
+            <span className="font-weight-bold">INDIA</span>Covid-19 Dashboard
+          </h1>
+        </div>
+        <div className="table-responsive">
+          <table clasName="table table-hover">
+            <thead className="thead-dark">
+              <tr>
+                <th>State</th>
+                <th>confirmed</th>
+                <th>recovered</th>
+                <th>deaths</th>
+                <th>active</th>
+                <th>updated</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Statewise;
